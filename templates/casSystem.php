381d5401c033f6933413f5533e6a8d734eca615a
dcarley <dcarley@bomgar.com>
2018-11-22 03:51:32 -0600

    logic to add user to db if they don't exist in the first place

--- templates/casSystem.php
+++ templates/casSystem.php
@@ -7,6 +7,22 @@
     $rootUri = $req->getRootUri();
     
     if(isset($_SESSION['phpCAS']['user'])){
+        $netId = $_SESSION['phpCAS']['user'];
+        $query = "SELECT username FROM users WHERE username = '$netId'";
+        $findUserQuery = $db->query($query)->fetchColumn();
+
+        //couldn't find user, so insert user into users table
+        if(!$findUserQuery){
+            $query = "INSERT INTO users (username, isAdmin) VALUES ('$netId', 0)";
+          
+           $db->query($query);
+          
+        } else {
+           //set user_id in session
+           $query = "SELECT id FROM users WHERE username = '$netId'";
+           $_SESSION['phpCAS']['user_id'] =  $db->query($query);
+        }
+
          header("Location: ". $rootUri . "/inventory");
          exit;
       } else {
