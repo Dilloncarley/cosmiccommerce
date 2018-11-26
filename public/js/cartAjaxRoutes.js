e32ff6fdfb926a60bd64cd896332355c53490536
dcarley <dcarley@bomgar.com>
2018-11-26 14:02:16 -0600

    ajax realtime update for cart counter

--- public/js/cartAjaxRoutes.js
+++ public/js/cartAjaxRoutes.js
@@ -13,28 +13,28 @@
                    
                     switch (res) {
                         case "0":
-                         console.log(res)
-                          M.toast({html: 'Something went wrong!', classes: 'failure-toast'}) 
-                            break;
+                            console.log(res)
+                            M.toast({html: 'Something went wrong!', classes: 'failure-toast'}) 
+                                break;
                         case "3":
-                          M.toast({html: 'Sorry that item is out of stock!', classes: 'failure-toast'}) 
+                            M.toast({html: 'Sorry that item is out of stock!', classes: 'failure-toast'}) 
 
-                          e.currentTarget.classList.add("disabled");
-                          var cardActionSection = e.currentTarget.offsetParent.offsetParent.getElementsByClassName("card-action")[0];
-                          var outOfStockBadge = '<span class="new badge red darken-2" data-badge-caption="out of stock"></span>';
-                          cardActionSection.innerHTML =  cardActionSection.innerHTML + outOfStockBadge;
+                            e.currentTarget.classList.add("disabled");
+                            var cardActionSection = e.currentTarget.offsetParent.offsetParent.getElementsByClassName("card-action")[0];
+                            var outOfStockBadge = '<span class="new badge red darken-2" data-badge-caption="out of stock"></span>';
+                            cardActionSection.innerHTML =  cardActionSection.innerHTML + outOfStockBadge;
 
-                          console.log(cardActionSection)
-                            break;
+                            console.log(cardActionSection)
+                                break;
                         default:
-                          var cartNotificationCount = document.getElementById('items-notification').getAttribute("data-badge-caption");
-                          var test = parseInt(cartNotificationCount)
-                          console.log(test)
-                          var cartNotification = document.getElementById('items-notification').setAttribute("data-badge-caption",  test +1 );
+                            var cartNotificationCount = document.getElementById('items-notification').getAttribute("data-badge-caption");
+                            var test = parseInt(cartNotificationCount)
+                            console.log(test)
+                            var cartNotification = document.getElementById('items-notification').setAttribute("data-badge-caption",  test +1 );
 
-                           M.toast({html: 'Item Added to Cart!', classes: 'success-toast'})
+                            M.toast({html: 'Item Added to Cart!', classes: 'success-toast'})
 
-                            break;
+                                break;
                     }
                     
                 }
